995684cbf2d10ca661b470a6323eb7c280d7844b
zjianru <864156480@qq.com>
2024-06-15 00:01:05 +0800

    finish client logic test

--- czConfig-demo/src/main/java/com/cz/czconfig/CzConfigDemoApplication.java
+++ czConfig-demo/src/main/java/com/cz/czconfig/CzConfigDemoApplication.java
@@ -1,5 +1,6 @@
 package com.cz.czconfig;
 
+import com.cz.config.client.annotation.EnableCzConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 
 @SpringBootApplication
 @EnableConfigurationProperties(LocalConfig.class)
+@EnableCzConfig
 public class CzConfigDemoApplication {
 
     @Value("${cz.config.key1}")
